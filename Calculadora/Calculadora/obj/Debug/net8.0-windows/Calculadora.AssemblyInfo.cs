253431fff90c60295b2438b2f8ae6503aafc46da
Xocolata <255942724+AbrilVilaltaEscabias@users.noreply.github.com>
2026-05-21 11:10:56 +0200

    canvi de nom

--- Calculadora/Calculadora/obj/Debug/net8.0-windows/Calculadora.AssemblyInfo.cs
+++ Calculadora/Calculadora/obj/Debug/net8.0-windows/Calculadora.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4190f509b0bda85552eda9410f1be0a0d183df1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddbcfc607543992efe88195057dff6df54574d0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
