ddbcfc607543992efe88195057dff6df54574d0a
Xocolata <255942724+AbrilVilaltaEscabias@users.noreply.github.com>
2026-05-21 11:01:22 +0200

    fotos

--- Calculadora/Calculadora/obj/Debug/net8.0-windows/App.g.cs
+++ Calculadora/Calculadora/obj/Debug/net8.0-windows/App.g.cs
@@ -47,7 +47,7 @@ namespace Calculadora {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.7.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.8.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -73,7 +73,7 @@ namespace Calculadora {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.7.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.8.0")]
         public static void Main() {
             Calculadora.App app = new Calculadora.App();
             app.InitializeComponent();
